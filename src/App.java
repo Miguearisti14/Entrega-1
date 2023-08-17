f281be88df43297e554beef069a650f1a178fc55
miguel <miguel.aristizabalp.col@upb.edu.co>
2023-08-17 10:12:03 -0500

    version2

--- src/App.java
+++ src/App.java
@@ -3,21 +3,33 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
         Scanner scanner = new Scanner(System.in);
+        int i;
+
+        do{
 
         System.out.println("Ingrese el numero 1");
         double userFirstOperator = scanner.nextDouble();
 
         System.out.println("Ingrese el numero 2");
         double userSecondOperator = scanner.nextDouble();
-
-        scanner.close();
+        
 
         System.out.println("Resultados:");
 
-        System.out.println(userFirstOperator+userSecondOperator);
-        System.out.println(userFirstOperator-userSecondOperator);
-        System.out.println(userFirstOperator*userSecondOperator);
-        System.out.println(userFirstOperator/userSecondOperator);
+        System.out.println("La suma es "+(userFirstOperator+userSecondOperator));
+        System.out.println("La resta es "+(userFirstOperator-userSecondOperator));
+        System.out.println("La multiplicacion es "+(userFirstOperator*userSecondOperator));
+        System.out.println("La division es "+(userFirstOperator/userSecondOperator));
+
+        System.out.println("Si desea realizar otro calculo presione 1. de lo contrario presione 0");
+        i = scanner.nextInt();
+
+        
+
+        }while(i!=0);
+
+        scanner.close();
+        System.out.println("Programa finalizado");
 
     }
 }
